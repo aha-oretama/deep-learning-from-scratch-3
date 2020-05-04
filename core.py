c73f4d2dec89548b0ef1ca2afed9944773ababf5
aha-oretama <sekine_y_529@msn.com>
2020-05-04 11:34:55 +0900

    step7

--- core.py
+++ core.py
@@ -5,6 +5,17 @@ class Variable:
     def __init__(self, data) -> None:
         self.data = data
         self.grad = None
+        self.creator = None
+
+    def set_creator(self, func):
+        self.creator = func
+
+    def backward(self):
+        f = self.creator
+        if f is not None:
+            x = f.input
+            x.grad = f.backward(self.grad)
+            x.backward()
 
 
 class Function:
@@ -12,7 +23,9 @@ class Function:
         x = input.data
         y = self.forward(x)
         output = Variable(y)
+        output.set_creator(self)
         self.input = input
+        self.output = output
         return output
 
     def forward(self, x):
