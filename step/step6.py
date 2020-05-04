cd5d557676c79507d6d1a2499e0f78ad1c7fc37f
aha-oretama <sekine_y_529@msn.com>
2020-05-04 10:57:02 +0900

    step6

---
+++ step/step6.py
@@ -0,0 +1,20 @@
+import numpy as np
+from core import Variable, Square, Exp
+
+A = Square()
+B = Exp()
+C = Square()
+
+x = Variable(np.array(0.5))
+a = A(x)
+b = B(a)
+y = C(b)
+
+print(y)
+
+y.grad = np.array(1.0)
+b.grad = C.backward(y.grad)
+a.grad = B.backward(b.grad)
+x.grad = A.backward(a.grad)
+print(x.grad)
+
