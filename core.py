ef3826c2882a547e6ec0a9023099ff2cb2eab682
aha-oretama <sekine_y_529@msn.com>
2020-05-06 12:29:58 +0900

    step17,18

--- core.py
+++ core.py
@@ -1,4 +1,24 @@
 import numpy as np
+import weakref
+import contextlib
+
+
+class Config:
+    enable_backprop = True
+
+
+@contextlib.contextmanager
+def using_config(name, value):
+    old_value = getattr(Config, name)
+    setattr(Config, name, value)
+    try:
+        yield
+    finally:
+        setattr(Config, name, old_value)
+
+
+def no_grad():
+    return using_config('enable_backprop', False)
 
 
 class Variable:
@@ -16,13 +36,14 @@ class Variable:
         self.creator = func
         self.generation = func.generation + 1
 
-    def backward(self):
+    def backward(self, retain_grad=False):
         if self.grad is None:
             self.grad = np.ones_like(self.data)
 
         funcs = []
         # 重複を除くためのみ。変数としては使わない
         seen_set = set()
+
         def add_func(f):
             if f not in seen_set:
                 funcs.append(f)
@@ -32,7 +53,9 @@ class Variable:
         add_func(self.creator)
         while funcs:
             f = funcs.pop()
-            gys = [output.grad for output in f.outputs]
+            # enable_backprop = False のときにここのメソッドが呼ばれるとundefinedになるが、
+            # 効率のためにそのチェックのための条件分岐を追加しない
+            gys = [output().grad for output in f.outputs]
             gxs = f.backward(*gys)
             if not isinstance(gxs, tuple):
                 gxs = (gxs, )
@@ -46,6 +69,10 @@ class Variable:
                 if x.creator is not None:
                     add_func(x.creator)
 
+            if not retain_grad:
+                for y in f.outputs:
+                    y().grad = None
+
     def clear_grad(self):
         self.grad = None
 
@@ -57,11 +84,13 @@ class Function:
         if not isinstance(ys, tuple):
             ys = (ys,)
         outputs = [Variable(as_array(y)) for y in ys]
-        self.generation = max([x.generation for x in inputs])
-        for output in outputs:
-            output.set_creator(self)
-        self.inputs = inputs
-        self.outputs = outputs
+
+        if Config.enable_backprop:
+            self.generation = max([x.generation for x in inputs])
+            for output in outputs:
+                output.set_creator(self)
+            self.inputs = inputs
+            self.outputs = [weakref.ref(output) for output in outputs]
         return outputs if len(outputs) > 1 else outputs[0]
 
     def forward(self, *xs):
