64f6a5cacd2761c690c947e899a85f782a202e11
aha-oretama <sekine_y_529@msn.com>
2020-05-12 00:16:26 +0900

    step38

--- dezero/core.py
+++ dezero/core.py
@@ -3,6 +3,8 @@ import weakref
 
 import numpy as np
 
+import dezero.functions
+
 
 class Config:
     enable_backprop = True
@@ -51,6 +53,23 @@ class Variable:
     @property
     def dtype(self):
         return self.data.dtype
+
+    def reshape(self, *shape):
+        if len(shape) == 1 and isinstance(shape[0], (tuple, list)):
+            shape = shape[0]
+        return dezero.functions.reshape(self, shape)
+
+    def transpose(self, *axes):
+        if len(axes) == 0:
+            axes = None
+        elif len(axes) == 1:
+            if isinstance(axes[0], (tuple, list) or axes[0] is None):
+                axes = axes[0]
+        return dezero.functions.transpose(self, axes)
+
+    @property
+    def T(self):
+        return dezero.functions.transpose(self)
 
     def __len__(self):
         return len(self.data)
