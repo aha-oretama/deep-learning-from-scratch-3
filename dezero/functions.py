ffc2705bfeac452bf5af21c897193f7238f5e4b3
aha-oretama <sekine_y_529@msn.com>
2020-05-16 16:47:18 +0900

    step41

--- dezero/functions.py
+++ dezero/functions.py
@@ -126,6 +126,19 @@ class SumTo(Function):
         return gx
 
 
+class MatMul(Function):
+
+    def forward(self, x, W):
+        y = x.dot(W)
+        return y
+
+    def backward(self, gy):
+        x, W = self.inputs
+        gx = matmul(gy, W.T)
+        gW = matmul(x.T, gy)
+        return gx, gW
+
+
 def square(x):
     return Square()(x)
 
@@ -170,3 +183,7 @@ def sum_to(x, shape):
     if x.shape == shape:
         return as_variable(x)
     return SumTo(shape)(x)
+
+
+def matmul(x, W):
+    return MatMul()(x, W)
