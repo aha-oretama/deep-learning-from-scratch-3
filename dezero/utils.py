0e45994ab92d8cc00e6095454742557908d40606
aha-oretama <sekine_y_529@msn.com>
2020-07-23 11:34:01 +0900

    step55

--- dezero/utils.py
+++ dezero/utils.py
@@ -227,3 +227,7 @@ def logsumexp(x, axis=1):
     np.log(s, out=s)
     m += s
     return m
+
+
+def get_conv_outsize(input_size, kernel_size, stride, pad):
+    return (input_size + pad * 2 - kernel_size) // stride + 1
